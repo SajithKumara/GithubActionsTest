c2a475146a2678dbee012ec9b2a5b0c9b6a4d571
sajithkumara <sajithkumara89@gmail.com>
2025-02-16 23:54:08 +0000

    update sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.coveragex.githubactionstest"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.coveragex.githubactionstest"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
