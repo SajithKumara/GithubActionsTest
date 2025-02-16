114227f4e6bba38f28beb8ff1ff12e96441b4324
sajithkumara <sajithkumara89@gmail.com>
2025-02-16 23:19:44 +0000

    change request check

--- app/src/main/java/com/coveragex/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/coveragex/githubactionstest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GithubActionsTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Github change",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
